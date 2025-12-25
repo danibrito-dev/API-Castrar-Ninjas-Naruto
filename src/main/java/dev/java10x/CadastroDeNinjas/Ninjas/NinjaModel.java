e43ff31dcd5286a7fa4d0e5819853c1e89c02333
@danibrito-dev <dev.danibrito@gmail.com>
2025-12-24 21:03:23 -0300

    Modelagem do BD - NinjaModel e MissoesModel - concluidos, anotations feitas para relacionamento de tabelas

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,6 +1,9 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 // O Entity transforma uma classe em uma entidade do BD!
 @Entity
@@ -13,6 +16,12 @@ public class NinjaModel {
     private String nome;
     private String email;
     private int idade;
+
+    // @ManyToOne => um ninja tem uma única missão.
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") // Foreing Key -> chave estrangeira
+    private MissoesModel missoes;
+
 
     public NinjaModel() {
     }
